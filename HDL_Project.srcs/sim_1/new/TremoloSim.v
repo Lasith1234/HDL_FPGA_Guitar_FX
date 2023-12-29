e9274ff782432d01c12b4e13389e91fb7ac23dc6
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-12-29 21:40:31 +0530

    tremolo effect works

--- HDL_Project.srcs/sim_1/new/TremoloSim.v
+++ HDL_Project.srcs/sim_1/new/TremoloSim.v
@@ -5,12 +5,14 @@ module TremoloSim;
   reg clk;
   reg rst;
   reg [15:0] guitar_input;
+  reg [15:0] rate;
   wire [15:0] tremolo_output;
 
   TremoloEffect uut (
     .clk(clk),
     .rst(rst),
     .guitar_input(guitar_input),
+    .rate(rate),
     .tremolo_output(tremolo_output)
   );
 
@@ -23,15 +25,12 @@ module TremoloSim;
     rst = 1;
     #10 rst = 0;
 
-    guitar_input = 16'h4000; 
+    rate = 16'd4;
 
-    #10 guitar_input = 16'h5000;
-    #100; 
+    guitar_input = 16'd3000; #200
+    guitar_input = 16'd10000; #200; 
     
-    #10 guitar_input = 16'h1000;
-    #100; 
-    
-    #1000 $finish;
+    #10 $finish;
   end
 
 endmodule
--- HDL_Project.srcs/sources_1/new/TremoloEffect.v
+++ HDL_Project.srcs/sources_1/new/TremoloEffect.v
@@ -4,32 +4,26 @@ module TremoloEffect (
   input wire clk,
   input wire rst,
   input wire [15:0] guitar_input,
+  input wire [15:0] rate,
   output wire [15:0] tremolo_output
 );
-  parameter DEPTH = 4'd4; 
-  parameter RATE = 16'b0000000000001010; 
 
   reg [15:0] tremolo_output_reg;
-  reg [15:0] tremolo_depth_reg;
-  reg [15:0] tremolo_rate_counter;
-  
-  wire [15:0] temp;
-  assign temp = tremolo_depth_reg;
+  reg [15:0] tremolo_counter;
 
   always @(posedge clk or posedge rst) begin
     if (rst) begin
       tremolo_output_reg <= 0;
-      tremolo_depth_reg <= 0;
-      tremolo_rate_counter <= 0;
+      tremolo_counter <= 0;
     end else begin
-      tremolo_depth_reg <= tremolo_depth_reg + DEPTH;
-      if (tremolo_depth_reg >= 16'd16) tremolo_depth_reg <= 0;
-
-      //tremolo_rate_counter <= tremolo_rate_counter + 1;
-      //if (tremolo_rate_counter == RATE) tremolo_rate_counter <= 0;
-
-      tremolo_output_reg <= guitar_input + tremolo_depth_reg;
-      //tremolo_output_reg <= guitar_input;
+      
+      tremolo_output_reg <= (guitar_input >> tremolo_counter);
+      tremolo_counter <= tremolo_counter + 1;
+      
+      if (tremolo_counter >= rate) begin
+        tremolo_counter <= 0;
+      end
+      
     end
   end
   assign tremolo_output = tremolo_output_reg;
